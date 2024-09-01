8967d8e0c1ea6a1eb69f55d8b6764c5bafbb1911
siraj yesuf <sirajyesuf762@gmail.com>
2024-09-01 23:26:41 +0300

    =tailwindcss installed

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import { Head } from "@inertiajs/inertia-react";
 
 export default function Home() {
-    return <h1>Welcome back</h1>;
+    return <h1 className="bg-red-500">Welcome back</h1>;
 }
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./resources/js/**/*.jsx"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
